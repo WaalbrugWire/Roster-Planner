4dc74ccaab5155e7c02dea7c6f01a193ca471069
ptpnl <ptpnl@hotmail.com>
2018-11-21 20:47:33 +0100

    move hliday calculaten
    moved th holiday calculation to the main UI panel

--- src/main/java/org/optaplanner/examples/nurserostering/domain/ShiftAssignment.java
+++ src/main/java/org/optaplanner/examples/nurserostering/domain/ShiftAssignment.java
@@ -19,10 +19,6 @@ package org.optaplanner.examples.nurserostering.domain;
 import java.time.DayOfWeek;
 
 import com.thoughtworks.xstream.annotations.XStreamAlias;
-import java.time.LocalDate;
-import java.time.Month;
-import java.time.ZoneId;
-import java.util.ArrayList;
 import org.optaplanner.core.api.domain.entity.PlanningEntity;
 import org.optaplanner.core.api.domain.variable.PlanningVariable;
 import org.optaplanner.examples.common.domain.AbstractPersistable;
@@ -32,167 +28,85 @@ import org.optaplanner.examples.nurserostering.domain.solver.MovableShiftAssignm
 import org.optaplanner.examples.nurserostering.domain.solver.ShiftAssignmentDifficultyComparator;
 
 @PlanningEntity(movableEntitySelectionFilter = MovableShiftAssignmentSelectionFilter.class,
-		difficultyComparatorClass = ShiftAssignmentDifficultyComparator.class)
-
+        difficultyComparatorClass = ShiftAssignmentDifficultyComparator.class)
 @XStreamAlias("ShiftAssignment")
-public class ShiftAssignment extends AbstractPersistable
-{
-	// Planning variables: changes during planning, between score calculations.
-	@PlanningVariable(valueRangeProviderRefs = 	{"employeeRange" },
-		strengthComparatorClass = EmployeeStrengthComparator.class)
-	
-	private Shift shift;
-	private int indexInShift;
-	private ArrayList<LocalDate> hollydays;
-	
-	private Employee employee;
+public class ShiftAssignment extends AbstractPersistable {
 
-	public Shift getShift()
-	{
-		return shift;
-	}
+    private Shift shift;
+    private int indexInShift;
 
-	public void setShift(Shift shift)
-	{
-		this.shift = shift;
-	}
+    // Planning variables: changes during planning, between score calculations.
+    @PlanningVariable(valueRangeProviderRefs = {"employeeRange"},
+            strengthComparatorClass = EmployeeStrengthComparator.class)
+    private Employee employee;
 
-	public int getIndexInShift()
-	{
-		return indexInShift;
-	}
+    public Shift getShift() {
+        return shift;
+    }
 
-	public void setIndexInShift(int indexInShift)
-	{
-		this.indexInShift = indexInShift;
-	}
+    public void setShift(Shift shift) {
+        this.shift = shift;
+    }
 
-	public Employee getEmployee()
-	{
-		return employee;
-	}
+    public int getIndexInShift() {
+        return indexInShift;
+    }
 
-	public void setEmployee(Employee employee)
-	{
-		this.employee = employee;
-	}
+    public void setIndexInShift(int indexInShift) {
+        this.indexInShift = indexInShift;
+    }
 
-	// ************************************************************************
-	// Complex methods
-	// ************************************************************************
-	public ShiftDate getShiftDate()
-	{
-		return shift.getShiftDate();
-	}
+    public Employee getEmployee() {
+        return employee;
+    }
 
-	public ShiftType getShiftType()
-	{
-		return shift.getShiftType();
-	}
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
 
-	public int getShiftDateDayIndex()
-	{
-		return shift.getShiftDate().getDayIndex();
-	}
+    // ************************************************************************
+    // Complex methods
+    // ************************************************************************
 
-	public DayOfWeek getShiftDateDayOfWeek()
-	{
-		return shift.getShiftDate().getDayOfWeek();
-	}
+    public ShiftDate getShiftDate() {
+        return shift.getShiftDate();
+    }
 
-	public Contract getContract()
-	{
-		if (employee == null)
-		{
-			return null;
-		}
-		return employee.getContract();
-	}
+    public ShiftType getShiftType() {
+        return shift.getShiftType();
+    }
 
-	public boolean isWeekend()
-	{
-		if (employee == null)
-			return false;
-		
-		// TODO: add hollyday calculation less hacky
-		calculateHollydays(shift.getShiftDate().getDate());
-		if (hollydays.contains(shift.getShiftDate().getDate()))
-			return true;
-		
-		WeekendDefinition weekendDefinition = employee.getContract().getWeekendDefinition();
-		DayOfWeek dayOfWeek = shift.getShiftDate().getDayOfWeek();
-		
-		return weekendDefinition.isWeekend(dayOfWeek);
-	}
+    public int getShiftDateDayIndex() {
+        return shift.getShiftDate().getDayIndex();
+    }
 
-	public int getWeekendSundayIndex()
-	{
-		return shift.getShiftDate().getWeekendSundayIndex();
-	}
+    public DayOfWeek getShiftDateDayOfWeek() {
+        return shift.getShiftDate().getDayOfWeek();
+    }
 
-	@Override
-	public String toString()
-	{
-		return shift.toString();
-	}
+    public Contract getContract() {
+        if (employee == null) {
+            return null;
+        }
+        return employee.getContract();
+    }
 
-	private void calculateHollydays(LocalDate _date)
-	{
-		hollydays = new ArrayList<>();
-		LocalDate easter = calculateEaster(_date.getYear());
-				
-		// nieuw jaar
-		hollydays.add(LocalDate.of(_date.getYear(), 01, 01));
+    public boolean isWeekend() {
+        if (employee == null) {
+            return false;
+        }
+        WeekendDefinition weekendDefinition = employee.getContract().getWeekendDefinition();
+        DayOfWeek dayOfWeek = shift.getShiftDate().getDayOfWeek();
+        return weekendDefinition.isWeekend(dayOfWeek);
+    }
 
-		// goede vrijdag
-		hollydays.add(easter.minusDays(2));
+    public int getWeekendSundayIndex() {
+        return shift.getShiftDate().getWeekendSundayIndex();
+    }
 
-		// paasen
-		hollydays.add(easter);
-		hollydays.add(easter.plusDays(1));
+    @Override
+    public String toString() {
+        return shift.toString();
+    }
 
-		// Koningsdag
-		hollydays.add(LocalDate.of(_date.getYear(), 04, 27));
-
-		// bevrijdingsdag
-		hollydays.add(LocalDate.of(_date.getYear(), 05, 05));
-
-		// hemelvaart
-		hollydays.add(easter.plusDays(39));
-
-		// pinksteren
-		hollydays.add(easter.plusDays(50));
-		hollydays.add(easter.plusDays(51));
-
-		// kerst	
-		hollydays.add(LocalDate.of(_date.getYear(), 12, 25));
-		hollydays.add(LocalDate.of(_date.getYear(), 12, 26));
-	}
-	
-	private LocalDate calculateEaster(int _year)
-	{
-		int day = 0;
-		int month = 0;
-		int golden = 0; // g is the position within the 19 year lunar cycle; known as the golden number
-		int century = 0; // the current century
-		int lunar = 0; //  days between the equinox and the next full moon
-		int i = 0; //  days between the full moon after the equinox and the first sunday after that full moon
-
-		golden = _year % 19;
-		century = _year / 100;
-		lunar = (century - (int)(century / 4) - (int)((8 * century + 13) / 25) + 19 * golden + 15) % 30;
-		i = lunar - (int)(lunar / 28) * (1 - (int)(lunar / 28) * (int)(29 / (lunar + 1)) * (int)((21 - golden) / 11));
-
-		day = i - ((_year + (int)(_year / 4) + i + 2 - century + (int)(century / 4)) % 7) + 28;
-		month = 3;
-
-		if (day > 31)
-		{
-			month++;
-			day -= 31;
-		}
-		
-		LocalDate date = LocalDate.of(_year, month, day);
-		return date;
-	}
 }
