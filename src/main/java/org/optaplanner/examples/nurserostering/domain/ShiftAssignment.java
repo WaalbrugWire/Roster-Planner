4b886f0adc541e4768ecd01abe7083f7e6407611
ptpnl <ptpnl@hotmail.com>
2018-11-25 16:26:34 +0100

    move Calculate Holidays to own class

--- src/main/java/org/optaplanner/examples/nurserostering/domain/ShiftAssignment.java
+++ src/main/java/org/optaplanner/examples/nurserostering/domain/ShiftAssignment.java
@@ -30,82 +30,99 @@ import org.optaplanner.examples.nurserostering.domain.solver.ShiftAssignmentDiff
 @PlanningEntity(movableEntitySelectionFilter = MovableShiftAssignmentSelectionFilter.class,
         difficultyComparatorClass = ShiftAssignmentDifficultyComparator.class)
 @XStreamAlias("ShiftAssignment")
-public class ShiftAssignment extends AbstractPersistable {
+public class ShiftAssignment extends AbstractPersistable
+{
 
     private Shift shift;
     private int indexInShift;
 
     // Planning variables: changes during planning, between score calculations.
-    @PlanningVariable(valueRangeProviderRefs = {"employeeRange"},
+    @PlanningVariable(valueRangeProviderRefs =
+    {
+        "employeeRange"
+    },
             strengthComparatorClass = EmployeeStrengthComparator.class)
     private Employee employee;
 
-    public Shift getShift() {
+    public Shift getShift()
+    {
         return shift;
     }
 
-    public void setShift(Shift shift) {
+    public void setShift(Shift shift)
+    {
         this.shift = shift;
     }
 
-    public int getIndexInShift() {
+    public int getIndexInShift()
+    {
         return indexInShift;
     }
 
-    public void setIndexInShift(int indexInShift) {
+    public void setIndexInShift(int indexInShift)
+    {
         this.indexInShift = indexInShift;
     }
 
-    public Employee getEmployee() {
+    public Employee getEmployee()
+    {
         return employee;
     }
 
-    public void setEmployee(Employee employee) {
+    public void setEmployee(Employee employee)
+    {
         this.employee = employee;
     }
 
     // ************************************************************************
     // Complex methods
     // ************************************************************************
-
-    public ShiftDate getShiftDate() {
+    public ShiftDate getShiftDate()
+    {
         return shift.getShiftDate();
     }
 
-    public ShiftType getShiftType() {
+    public ShiftType getShiftType()
+    {
         return shift.getShiftType();
     }
 
-    public int getShiftDateDayIndex() {
+    public int getShiftDateDayIndex()
+    {
         return shift.getShiftDate().getDayIndex();
     }
 
-    public DayOfWeek getShiftDateDayOfWeek() {
+    public DayOfWeek getShiftDateDayOfWeek()
+    {
         return shift.getShiftDate().getDayOfWeek();
     }
 
-    public Contract getContract() {
-        if (employee == null) {
+    public Contract getContract()
+    {
+        if (employee == null)
             return null;
-        }
+        
         return employee.getContract();
     }
 
-    public boolean isWeekend() {
-        if (employee == null) {
+    public boolean isWeekend()
+    {
+        if (employee == null)
             return false;
-        }
+
         WeekendDefinition weekendDefinition = employee.getContract().getWeekendDefinition();
         DayOfWeek dayOfWeek = shift.getShiftDate().getDayOfWeek();
         return weekendDefinition.isWeekend(dayOfWeek);
     }
 
-    public int getWeekendSundayIndex() {
+    public int getWeekendSundayIndex()
+    {
         return shift.getShiftDate().getWeekendSundayIndex();
     }
 
     @Override
-    public String toString() {
+    public String toString()
+    {
         return shift.toString();
     }
 
